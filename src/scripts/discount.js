09ffa0e2efb3b7c731a9e4c8f89c7c08ff4271ad
Joshua Gugun Siagian <suabahasa@gmail.com>
2024-08-16 03:12:04 +0800

    update hidden discount

--- src/scripts/discount.js
+++ src/scripts/discount.js
@@ -1,5 +1,5 @@
 console.log(
-    `%cYabe Webfont 🎉%c You just found a hidden coupon code! Use %cBROWSER-CONSOLE%c to get 40% off on your purchase`,
+    `%cYabe Webfont 🎉%c You just found a hidden coupon code! Use %cBROWSER-CONSOLE%c to get 15% off on your purchase`,
     "background: #fccb5b; color: black; padding: 2px 4px; border-radius: 2px; font-size: 12px;",
     '',
     "background: #717171; color: white; padding: 2px 3px; border-radius: 2px; font-size: 0.8em;",
